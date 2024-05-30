e8bb18d8edc61a85c4b18f40ce56f3251270b4af
Dumbrovsky <148480956+Dumbrovsky@users.noreply.github.com>
2024-05-30 19:40:13 +0200

    p_car.py

--- project_race/green_flag/p_car.py
+++ project_race/green_flag/p_car.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.math import Vector2
 
+
+
 class RaceCar(pygame.sprite.Sprite):
     def __init__(self, track_mask):
         super().__init__()
@@ -9,35 +11,30 @@ class RaceCar(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (350, 500)
         self.speed = 5
-        self.velocity = Vector2(1, 0)  # Původní směr vozidla (vpravo)
+        self.velocity = Vector2(1, 0)
         self.track_mask = track_mask
         self.last_valid_position = self.rect.center
 
     def update(self):
         keys = pygame.key.get_pressed()
-        rotation_speed = 5  # Rychlost otáčení auta
+        rotation_speed = 5  
 
-        # Změna směru vozidla podle stisknutých kláves
         if keys[pygame.K_LEFT]:
             self.velocity.rotate_ip(-rotation_speed)
         if keys[pygame.K_RIGHT]:
             self.velocity.rotate_ip(rotation_speed)
 
-        # Pohyb vozidla
         if keys[pygame.K_UP]:
             self.rect.move_ip(self.velocity * self.speed)
 
-        # Omezení pohybu vozidla na trajektorii trati
         if not self.track_mask.get_at((int(self.rect.centerx), int(self.rect.centery))):
-            self.rect.center = (500, 500)  # Vrátit auto na předdefinovanou pozici
-            self.velocity = Vector2(1, 0)  # Resetovat směr pohybu
+            self.rect.center = (500, 500)  
+            self.velocity = Vector2(1, 0) 
 
         self.last_valid_position = self.rect.center
 
-        # Otočení obrázku podle směru pohybu
-        angle = self.velocity.angle_to(Vector2(0, -1))  # Vypočítat úhel mezi aktuálním směrem a směrem nahoru
+        angle = self.velocity.angle_to(Vector2(0, -1)) 
         self.image = pygame.transform.rotate(self.original_image, angle)
-        self.rect = self.image.get_rect(center=self.rect.center)  # Udržovat pozici vozidla
+        self.rect = self.image.get_rect(center=self.rect.center) 
 
-        # Omezení pohybu vozidla do hranic okna (volitelné)
         self.rect.clamp_ip(pygame.display.get_surface().get_rect())
